c9438d790aede313d82b2ba4a2f22648f7139414
Nathan <nathanjohnhaight@gmail.com>
2025-10-02 11:53:01 -0600

    Models have been made and migrated

--- destinations/core/models.py
+++ destinations/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.TextField()
+    email = models.TextField(unique=True)
+    password_hash = models.TextField()
+
+class Session(models.Model):
+    user = models.OneToOneField("User", on_delete=models.CASCADE, primary_key=True)
+    token = models.TextField()
+
+class Destination(models.Model):
+    name = models.TextField()
+    review = models.TextField()
+    rating = models.PositiveIntegerField()
+    share_publicly = models.BooleanField()
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+
--- destinations/destinations/settings.py
+++ destinations/destinations/settings.py
@@ -32,21 +32,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'core.apps.CoreConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -60,8 +52,6 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
