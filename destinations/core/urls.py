7f364c88412d7be636aa30ca116fcb5d4bc684de
Nathan <nathanjohnhaight@gmail.com>
2025-10-03 12:02:09 -0600

    You can get to the create account page

--- destinations/core/urls.py
+++ destinations/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path("", views.index, name="index")
+   path("", views.index, name="index"),
+   path("user/new", views.create_account, name="create_account"),
 ]
--- destinations/core/views.py
+++ destinations/core/views.py
@@ -4,3 +4,6 @@ from django.http import HttpRequest
 # Create your views here.
 def index(req: HttpRequest):
     return render(req, "core/index.html")
+
+def create_account(req: HttpRequest):
+    return render(req, "core/create_account.html")
