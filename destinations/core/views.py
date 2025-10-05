070b280cf0f2a03270a41c311d44c45abec3c6db
Nathan <nathanjohnhaight@gmail.com>
2025-10-04 22:26:31 -0600

    Some create account stuff implmented

--- destinations/core/views.py
+++ destinations/core/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
+from django.contrib.auth.hashers import make_password, check_password
 
-# Create your views here.
 def index(req: HttpRequest):
     return render(req, "core/index.html")
 
 def create_account(req: HttpRequest):
     return render(req, "core/create_account.html")
+
+def user(req: HttpRequest):
+    query = req.POST
+    name = query["name"]
+    email = query["email"]
+    password_unhashed = query["password"]
+    if len(name) == 0:
+        return
+    print(name, email, password_unhashed)
+    return redirect("/")
