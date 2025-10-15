7ae0d2980eea7e3b18c376e6e1c091e354f3d210
Nathan <nathanjohnhaight@gmail.com>
2025-10-15 00:07:45 -0600

    You can longer edit other's destination records

--- destinations/core/middleware.py
+++ destinations/core/middleware.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect
 from django.http import HttpRequest
-from .models import Session, User
+from .models import Session, Destination
 
 def authentication_middleware(next):
     #Gets the "session_token" cookie and if there isn't one it the token is replaced by an empty string
@@ -8,19 +8,38 @@ def authentication_middleware(next):
         session_uris = ["/destinations", "/destinations/new"]
         uri = req.get_full_path()
         token = req.COOKIES.get("session_token", "")
-        req.signed_in = False
+        id = get_id(uri)
+        if id:
+            id = int(id)
 
         #Since empty strings are falsy it will skip over getting the user if there is no token
         if token:
-            session = Session.objects.get(token=token)
-            req.user = session.user
-            req.signed_in = True
+            session = Session.objects.get(token = token)
+            user = session.user
+            req.user = user
+
+            if id:
+                destination = Destination.objects.get(id = id)
+                print(id)
+                if destination.user != user:
+                    return redirect("/destinations")
 
         #If you don't have a session then it checks to make sure you can actually go where you requested
-        elif uri in session_uris:
+        elif uri in session_uris or id != None:
             return redirect("/session/new")
         
         res = next(req)
         return res
     
     return middleware
+
+def get_id(uri):
+    id = ""
+    for char in uri:
+        if char.isdigit():
+            id = id + char
+            print(id)
+    if len(id) != 0:
+        return id
+    
+    return None
